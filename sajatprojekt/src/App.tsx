03dc802c64eed1454ac329faee180a523b1afe32
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2024-02-03 16:15:28 +0100

    Az app fájlban a map nem tetszik neki azt még meg kell nézni

--- sajatprojekt/src/App.tsx
+++ sajatprojekt/src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css'
+import { User, Users } from "./user.ts";
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [users,setUser]=useState([] as User[])
+
+  useEffect(()=>{
+    async function load() {
+      try {
+        const response=await fetch('http://localhost:3000/bejelentkezesadatok')
+        const users=await response.json() as Users;
+        setUser(users.users)
+        
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    load()
+  },[])
+
   return (
     <>
-      
+    <div>
+      <ul>
+          {
+            users.map(user=><li>{user.name} {user.email}</li>)
+          }
+      </ul>
+    </div>
     </>
   )
 }
